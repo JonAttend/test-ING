1d2263f7558bee926a760cfc9902edc16ba6b434
Jonathan <jonathan.de-boisvilliers@sogeti.com>
2022-09-07 17:11:17 +0200

    fisrt step home done

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './styles.scss';
 
 const Home = () =>
@@ -6,9 +7,18 @@ const Home = () =>
         <div className="blockLeft"
             style={{ backgroundImage: `url('${process.env.PUBLIC_URL + '/img/hp_referral_visuelshowcase.webp'}')` }}
         >
+            <div className='blockLeft__content'>
+                <p><span>Lorem ipsum dolor sit amet</span></p>
+                <p><span>Lorem ipsum</span></p>
+            </div>
         </div>
         <div className="blockRight">
-            
+            <div className='blockRight__content'>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </p>
+                <Link to="/exercice2">eventuel lien</Link>
+            </div>
         </div>
     </div>
 
